5331bf49cf808b6d74d429f6086f457252288c5d
Kanhaiya mahto <110100023+kanhaiya062@users.noreply.github.com>
2024-03-05 01:52:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Roman Number to Integer/roman-number-to-integer.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String roman = br.readLine().trim();
+            Solution ob = new Solution();
+            System.out.println(ob.romanToDecimal(roman));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Finds decimal value of a given roman numeral
+    public int romanToDecimal(String str) {
+        // code here
+         HashMap<Character, Integer> romanToIntMap = new HashMap<>();
+        romanToIntMap.put('I', 1);
+        romanToIntMap.put('V', 5);
+        romanToIntMap.put('X', 10);
+        romanToIntMap.put('L', 50);
+        romanToIntMap.put('C', 100);
+        romanToIntMap.put('D', 500);
+        romanToIntMap.put('M', 1000);
+        
+        int result=0;
+        int prevValue=0;
+        
+        for (int i = str.length() - 1; i >= 0; i--) {
+            int curv = romanToIntMap.get(str.charAt(i));
+
+            if (curv < prevValue) {
+                result -= curv;
+            } 
+            else {
+                result += curv;
+            }
+            prevValue = curv;
+        }
+
+        return result;
+    }
+}
